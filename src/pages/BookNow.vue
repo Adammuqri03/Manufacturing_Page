68ae3e3b0a7f6df470ba42df3d8fd92c7c183858
Kurotsuchi <126145510+hitsuga13@users.noreply.github.com>
2024-05-22 17:40:21 +0800

    book update

--- src/pages/BookNow.vue
+++ src/pages/BookNow.vue
@@ -1,45 +1,234 @@
 <template>
-  <div class="container">
-    <div class="q-pa-md form-container">
-      <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
-        <q-input
-          filled
-          v-model="name"
-          label="Email *"
-          hint="Example@gmail.com"
-          lazy-rules
-          :rules="[
-            (val) =>
-              (val && val.length > 0) || 'Please insert valid information',
-          ]"
-        />
-
-        <q-input
-          filled
-          type="number"
-          v-model="age"
-          label="Quantity *"
-          lazy-rules
-          :rules="[
-            (val) =>
-              (val !== null && val !== '') || 'Please insert the quantity',
-            (val) => (val > 0 && val < 100) || 'Please insert valid quantity',
-          ]"
-        />
-
-        <q-toggle v-model="accept" label="I accept the license and terms" />
-
-        <div>
-          <q-btn label="Submit" type="submit" color="primary" />
-          <q-btn
-            label="Reset"
-            type="reset"
-            color="primary"
-            flat
-            class="q-ml-sm"
+  <div>
+    <header class="header" data-header style="background-color: green">
+      <div class="container">
+        <a href="#" class="logo">
+          <img
+            src="/img/myces_logo.png"
+            width="74"
+            height="24"
+            alt="home"
+            class="logo-light"
           />
-        </div>
-      </q-form>
+
+          <img
+            src="/logo/myces_logo.png"
+            width="74"
+            height="24"
+            alt="home"
+            class="logo-dark"
+          />
+        </a>
+
+        <nav class="navbar" data-navbar>
+          <div class="navbar-top">
+            <a href="#" class="logo">
+              <img
+                src="/logo/myces_logo.png"
+                width="74"
+                height="24"
+                alt="Home"
+              />
+            </a>
+
+            <button
+              class="nav-close-btn"
+              aria-label="close menu"
+              data-nav-toggler
+            >
+              <q-icon name="close-outline" aria-hidden="true"></q-icon>
+            </button>
+          </div>
+
+          <!--navbar/menu section-->
+          <ul class="navbar-list">
+            <li>
+              <a href="#" class="navbar-link" style="color: white">Home</a>
+            </li>
+
+            <li>
+              <a href="#" class="navbar-link" style="color: white">About</a>
+            </li>
+
+            <li>
+              <a
+                href="#"
+                class="navbar-link"
+                style="color: white"
+                @mouseover="showServiceMenu = true"
+                @mouseleave="showServiceMenu = false"
+                >Service
+                <q-menu
+                  style="
+                    width: 280px;
+                    background-color: rgba(0, 0, 0, 0.721);
+                    font-size: 18px;
+                  "
+                  class="text-white"
+                  v-model="showServiceMenu"
+                >
+                  <q-list
+                    @mouseover="showServiceMenu = true"
+                    @mouseleave="showServiceMenu = false"
+                  >
+                    <q-item class="item_Hover">
+                      <q-item-section>
+                        <a href="#">Energy Audit</a>
+                      </q-item-section>
+                    </q-item>
+                    <q-item class="item_Hover">
+                      <q-item-section
+                        ><a href="#">Monitoring System (MyCES EMARS)</a>
+                      </q-item-section>
+                    </q-item>
+                    <q-item class="item_Hover">
+                      <q-item-section>
+                        <a href="#">Measurement and Verification (M&V)</a>
+                      </q-item-section>
+                    </q-item>
+                    <q-item class="item_Hover">
+                      <q-item-section
+                        ><a href="#"
+                          >Sustainable Energy Management System(SEMS)</a
+                        >
+                      </q-item-section>
+                    </q-item>
+                    <q-item class="item_Hover">
+                      <q-item-section>
+                        <a hreg="#"> Awareness Training</a></q-item-section
+                      >
+                    </q-item>
+                    <q-item class="item_Hover">
+                      <q-item-section
+                        ><a href="#">Equipment Rental</a></q-item-section
+                      >
+                    </q-item>
+                    <q-item class="item_Hover">
+                      <q-item-section
+                        ><a href="#">Smart Home Automation</a></q-item-section
+                      >
+                    </q-item>
+                    <q-item class="item_Hover">
+                      <q-item-section><a href="#">AMARS</a></q-item-section>
+                    </q-item>
+                  </q-list>
+                </q-menu></a
+              >
+            </li>
+
+            <li>
+              <a
+                href="#"
+                class="navbar-link"
+                style="color: white"
+                @mouseover="showSubMenu = true"
+                @mouseleave="showSubMenu = false"
+                >Subsidiaries<q-menu
+                  style="
+                    width: 200px;
+                    background-color: rgba(0, 0, 0, 0.721);
+                    font-size: 18px;
+                  "
+                  class="text-white"
+                  v-model="showSubMenu"
+                >
+                  <q-list
+                    @mouseover="showSubMenu = true"
+                    @mouseleave="showSubMenu = false"
+                  >
+                    <q-item class="item_Hover">
+                      <q-item-section
+                        ><a href="https://biomed.mycesgroup.com/"
+                          >MYCES Biomedical Engineering Sdn Bhd</a
+                        ></q-item-section
+                      >
+                    </q-item>
+                    <q-item class="item_Hover">
+                      <q-item-section
+                        ><a href="#">MYCES AgroFarm Sdn Bhd</a></q-item-section
+                      >
+                    </q-item>
+                    <q-item class="item_Hover">
+                      <q-item-section
+                        ><a href="#"
+                          >MYCES Manufacturing Sdn Bhd</a
+                        ></q-item-section
+                      >
+                    </q-item>
+                    <q-item class="item_Hover">
+                      <q-item-section
+                        ><a href="#">MYCES Edutect Sdn Bhd</a></q-item-section
+                      >
+                    </q-item>
+                    <q-item class="item_Hover">
+                      <q-item-section
+                        ><a href="#"
+                          >MYCES Aircond & Electrical Services Sdn Bhd</a
+                        ></q-item-section
+                      >
+                    </q-item>
+                  </q-list>
+                </q-menu></a
+              >
+            </li>
+
+            <li>
+              <a href="#" class="navbar-link" style="color: white">Info</a>
+            </li>
+          </ul>
+        </nav>
+
+        <a href="#" class="btn btn-primary">Contact us</a>
+
+        <button class="nav-open-btn" aria-label="open menu" data-nav-toggler>
+          <q-icon name="menu-outline" aria-hidden="true"></q-icon>
+        </button>
+
+        <div class="overlay" data-nav-toggler data-overlay></div>
+      </div>
+    </header
+    <div class="container">
+      <div class="q-pa-md form-container">
+        <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+          <q-input
+            filled
+            v-model="name"
+            label="Email *"
+            hint="Example@gmail.com"
+            lazy-rules
+            :rules="[
+              (val) =>
+                (val && val.length > 0) || 'Please insert valid information',
+            ]"
+          />
+
+          <q-input
+            filled
+            type="number"
+            v-model="age"
+            label="Quantity *"
+            lazy-rules
+            :rules="[
+              (val) =>
+                (val !== null && val !== '') || 'Please insert the quantity',
+              (val) => (val > 0 && val < 100) || 'Please insert valid quantity',
+            ]"
+          />
+
+          <q-toggle v-model="accept" label="I accept the license and terms" />
+
+          <div>
+            <q-btn label="Submit" type="submit" color="primary" />
+            <q-btn
+              label="Reset"
+              type="reset"
+              color="primary"
+              flat
+              class="q-ml-sm"
+            />
+          </div>
+        </q-form>
+      </div>
     </div>
   </div>
 </template>
